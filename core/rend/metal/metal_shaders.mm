c8cb43958015925348302733ab958d9b381cc720
Isaac Marovitz <isaacryu@icloud.com>
2025-06-01 16:52:31 -0400

    Fix rotate 90 orientation
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- core/rend/metal/metal_shaders.mm
+++ core/rend/metal/metal_shaders.mm
@@ -499,7 +499,7 @@ vertex VertexOut vs_main(VertexIn in [[stage_in]]) {
     if (!rotate_quad) {
         out.position = float4(in.pos, 1.0);
     } else {
-        out.position = float4(in.pos.y, -in.pos.x, in.pos.z, 1.0);
+        out.position = float4(-in.pos.y, in.pos.x, in.pos.z, 1.0);
     }
 
     out.uv = in.uv;
